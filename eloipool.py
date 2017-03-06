0b3aad396648921cc470c08efc4f8ef375e4ea9d
martinmedina <tinnmedina@gmail.com>
2017-03-06 19:22:51 -0300

    version tracking added

--- eloipool.py
+++ eloipool.py
@@ -1043,6 +1043,7 @@ if __name__ == "__main__":
 
 	configLogger = logging.getLogger("ConfigLogger")
 	configFile = open("config.py", "r")
+	configLogger.info("ROOTSTOCK: version 1.0.1")
 	configLogger.info("ROOTSTOCK: config.py_log_start \n" + configFile.read())
 	configFile.close()
 	configLogger.info("ROOTSTOCK: config.py_log_complete")
